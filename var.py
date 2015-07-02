62524750653a357034228e408911d85c3fa7357d
Cas Widdershoven <caswiddershoven@gmail.com>
2015-07-02 16:46:28 +0200

    Raise error if you ask the value of an unset variable

--- var.py
+++ var.py
@@ -7,4 +7,5 @@ class Var(Expr):
 	def value(self, **kwargs):
 		if kwargs is not None and self.key in kwargs.keys():
 			return kwargs[self.key]
-			
+		else:
+			raise UnboundLocalError("Tried to find the value of the variable {}, but no value has been set.".format(self.key))
